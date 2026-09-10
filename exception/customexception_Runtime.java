5eb47ffff1cb0614cc89f05f1d57389e55f9976f
sumitc0144 <sumitc0144@gmail.com>
2026-09-10 13:26:41 +0530

    Java- interfaces Queue

--- exception/customexception_Runtime.java
+++ exception/customexception_Runtime.java
@@ -1,50 +1,49 @@
 package exception;
 
-
-
-//without thows keyword
-class PlayerDeadException extends RuntimeException{
-	public PlayerDeadException(String msg) {
-		super(msg);
-	}
+// Custom unchecked exception (RuntimeException)
+class PlayerDeadException extends RuntimeException {
+    public PlayerDeadException(String msg) {
+        super(msg);
+    }
 }
 
-class player{
-	private String name;
-	private int health;
-	
-	public player(String name,int health) {
-		this.name=name;
-		this.health=health;
-	}
-	
-	public void tokedamage(int damage) {
-		health-=damage;
-		
-		if(health<=0) {
-			throw new PlayerDeadException(this.name+" is Dead!!");
-		}
-	}
-	public int gethealth() {
-		return this.health;
-	}
-		
+// Player class
+class Player1 {
+    private String name;
+    private int health;
+
+    public Player1(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    // Method that can throw PlayerDeadException
+    public void takeDamage(int damage) throws PlayerDeadException {
+        health -= damage;
+
+        if (health <= 0) {
+            throw new PlayerDeadException(this.name + " is Dead!!");
+        }
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
 }
-	
 
-
+// Main class
 public class customexception_Runtime {
+    public static void main(String[] args) {
+        Player1 first = new Player1("Tony", 100);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		player first=new player("tony",100);
-		
-		try {
-			first.tokedamage(100);
-		}catch(PlayerDeadException e) {
-			System.out.println("Exception occur"+e);
-		}
+        try {
+            first.takeDamage(100); // This will trigger the exception
+        } catch (PlayerDeadException e) {
+            // Option 1: Print full exception (class + message)
+            System.out.println("Exception occur: " + e);
 
-	}
-
+            // Option 2: Print only the custom message
+            System.out.println("Exception occur: " + e.getMessage());
+        }
+    }
 }
