fba259aee7a04d3b0aac36d7a59b821418e7c7c8
sumitc0144 <sumitc0144@gmail.com>
2026-09-22 10:28:38 +0530

    Lambda expression Consumer

---
+++ LambdaExpression/Functions_Consumer.java
@@ -0,0 +1,36 @@
+package LambdaExpression;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+//Represents an operation that takes one input and returns nothing.
+//Abstract method:
+//	void accept(T t);
+//		You use it when you want to perform an action on a value (like printing, logging, saving),
+//		but don’t need to return anything.
+public class Functions_Consumer {
+
+	public static void main(String[] args) {
+		// Simple
+		 Consumer<String> printer = s -> System.out.println(s);
+
+	        printer.accept("Hello Consumer!");  // Output: Hello Consumer!
+	        
+//	     Consume with List
+	        List<String> names = Arrays.asList("Sumit", "Anita", "Ravi");
+
+	        Consumer<String> printUpper = s -> System.out.println(s.toUpperCase());
+
+	        names.forEach(printUpper);  
+	        
+//	        Chaining Consumers
+//	        Consumers have a default method andThen() that lets you chain multiple actions:
+	        Consumer<String> print = s -> System.out.println("Name: " + s);
+	        Consumer<String> length = s -> System.out.println("Length: " + s.length());
+
+	        Consumer<String> combined = print.andThen(length);
+
+	        combined.accept("Sumit");
+	}
+
+}
